301a0dc1e3560f87415932978b1966c48459358b
Miguel Ortiz | WebDev <elsuperm2016@gmail.com>
2025-04-30 10:30:25 -0500

    Modificacion archivo

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,36 @@
-// import { useState } from "react";
-// import reactLogo from "./assets/react.svg";
-// import viteLogo from "/vite.svg";
-import "./App.css";
-
+import { useState } from "react";
+import { GlobalStyles } from "@mui/material";
+import { MainPage } from "./pages/MainPage/MainPage";
+import { AboutUsPage } from "./pages/AboutUs/AboutUsPage";
+import { ProductsListPage } from "./pages/Products/ProductsListPage";
+import { ProductsDetailsPage } from "./pages/Products/ProductDetails";
+import { CarPage } from "./pages/Car/CarPage";
+import { RegisterPage } from "./pages/Register/Register";
 function App() {
   return (
     <>
-      <div className="app-container">
-        <header className="navbar">
-          <div className="navbar-logo">
-            <img
-              className="image-logo"
-              src="/public/logo.jpg"
-              alt="Logo papeleria"
-            />
-          </div>
-          <div className="search">
-            <label htmlFor="search">
-              <img
-                src="https://cdn-icons-png.flaticon.com/512/3721/3721746.png"
-                alt="icono lupa"
-              />
-            </label>
-            <input type="text" />
-          </div>
-          <nav className="navbar-links">
-            <a href="#">Inicio</a>
-            <a href="#">Productos</a>
-            <a href="#">Carrito</a>
-            <a href="#">Nosotros</a>
-            <a href="#">Iniciar sesión</a>
-          </nav>
-        </header>
-
-        <main className="main-content">
-          <h1>Productos Destacados</h1>
-          <p>Encuentra los mejores productos al mejor precio</p>
-
-          <div className="content-products">
-            <div className="filter-product">
-              <p>Filtros producto</p>
-            </div>
-            <div className="views-product">
-              <div></div>
-              <p>Vista producto</p>
-            </div>
-          </div>
-        </main>
-
-        <footer className="footer">
-          <p>&copy; 2025 Papelería. Todos los derechos reservados.</p>
-        </footer>
-      </div>
+      <GlobalStyles
+        styles={{
+          "*": {
+            margin: 0,
+            padding: 0,
+          },
+          body: {
+            fontFamily: "Roboto, sans-serif",
+            backgroundColor: "#f5f5f5",
+          },
+          main: {
+            flexGrow: 1,
+          },
+        }}
+      />
+      <MainPage />
+      {/* <AboutUsPage /> */}
+      {/* <ProductsListPage /> */}
+      {/* <ProductsDetailsPage /> */}
+      {/* <CarPage />
+       */}
+      {/* <RegisterPage /> */}
     </>
   );
 }
