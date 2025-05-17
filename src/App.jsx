424ac38519a0f11020b6c98a92e415a35398df12
Miguel Ortiz | WebDev <elsuperm2016@gmail.com>
2025-05-17 18:32:04 -0500

    ruta de importancion del app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,42 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { GlobalStyles } from "@mui/material";
-import { MainPage } from "./pages/MainPage/MainPage";
-import { AboutUsPage } from "./pages/AboutUs/AboutUsPage";
-import { ProductsListPage } from "./pages/Products/ProductsListPage";
-import { ProductsDetailsPage } from "./pages/Products/ProductDetails";
-import { CarPage } from "./pages/Car/CarPage";
-import { LoginPage } from "./pages/Login/LoginPage";
+import { MainPage } from "../src/pages/MainPage/MainPage";
+import { AboutUsPage } from "../src/pages/AboutUs/AboutUsPage";
+import { ProductListPage } from "../src/pages/Products/ProductsListPage";
+import { ProductDetailsPage } from "../src/pages/Products/ProductDetails";
+import { CarPage } from "../src/pages/Car/CarPage";
+import { LoginPage } from "../src/pages/Login/LoginPage";
 
 function App() {
   return (
     <>
       <BrowserRouter>
-      <GlobalStyles
-        styles={{
-          "*": { margin: 0, padding: 0, boxSizing: "border-box" },
-          body: {
-            fontFamily: "Inter, Roboto, 'Helvetica Neue', Arial, sans-serif",
-            backgroundColor: "#f5f8fa",
-            color: "#222",
-          },
-          main: { flexGrow: 1 },
-        }}
-      />
+        <GlobalStyles
+          styles={{
+            "*": { margin: 0, padding: 0, boxSizing: "border-box" },
+            body: {
+              fontFamily: "Inter, Roboto, 'Helvetica Neue', Arial, sans-serif",
+              backgroundColor: "#f5f8fa",
+              color: "#222",
+            },
+            main: { flexGrow: 1 },
+          }}
+        />
 
         {/* Rutas para las paginas */}
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/about" element={<AboutUsPage />} />
-        <Route path="/products" element={<ProductsListPage />} />
-        <Route path="/product" element={<ProductsDetailsPage />} />
-        <Route path="/cart" element={<CarPage />} />
-        <Route path="/login" element={<LoginPage />} />
-      </Routes>
-    </BrowserRouter>
-    
-      
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/about" element={<AboutUsPage />} />
+          <Route path="/products" element={<ProductListPage />} />
+          <Route
+            path="/products/:productName"
+            element={<ProductDetailsPage />}
+          />
+          <Route path="/cart" element={<CarPage />} />
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
+      </BrowserRouter>
+
       {/* <AboutUsPage /> */}
       {/* <ProductsListPage /> */}
       {/* <ProductsDetailsPage /> */}
